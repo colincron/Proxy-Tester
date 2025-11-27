ed7b62e6e8e87c1a2e03bc768d9a34bfcec4a8a9
colin <colint.cron@gmail.vcom>
2025-11-26 20:43:19 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from urllib.request import urlretrieve
+import requests
+import urllib3.exceptions
+
+test_http_site = 'http://httpforever.com'
+
+def get_file():
+    url = "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/http/data.txt"
+    urlretrieve(url, 'data.txt')
+
+def http_proxy_test(ip_address, port):
+    ip_port_str = ip_address + ":" + str(port)
+    proxies = {
+        'http': ip_port_str,
+        }
+    try:
+        response = requests.get(test_http_site, proxies=proxies)
+        if response.status_code == 200:
+            print("[+] This one works: " + "http://" + ip_address + ":" + str(port))
+        else:
+            print("[-] This one doesn't "+ "http://" + ip_address + ":" + str(port) )
+    except ConnectionResetError:
+        print("[-] Connection reset by peer. Connection Reset Error")
+    except urllib3.exceptions.MaxRetryError:
+        print("[-] Max retries. MaxRetryError")
+    except requests.exceptions.ProxyError:
+        print("[-] Max retries exceeded. requests.exceptions.ProxyError")
+    except requests.exceptions.TooManyRedirects:
+        print("[-] Too many redirects. requests.exceptions.TooManyRedirects")
+
+
+def file_handler():
+    get_file()
+    with open('data.txt') as lines:
+        for line in lines:
+            ip = line.replace("http://","").split(":")[0]
+            port = line.replace("http://","").split(":")[1]
+            http_proxy_test(ip, port)
+
+file_handler()
